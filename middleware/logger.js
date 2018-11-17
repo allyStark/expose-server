5787800e0478de7a92285523911ae6b559fbb356
roboticsound <roboticsound@tutanota.com>
2018-11-17 17:46:07 +0000

    Create logs folder

--- middleware/logger.js
+++ middleware/logger.js
@@ -5,6 +5,9 @@ module.exports = (req, res, next) => {
     let log = `${now}: ${req.method} ${req.url}`
 
     console.log(log);
+    fs.mkdir('./logs', { recursive: true }, (err) => {
+        if (err) throw err;
+    });
     fs.appendFile('./logs/server.log', log + '\n', (err) => {
         if (err) {
             console.log(new Error(err));
